e0cbdf72682a6327a2eb46a88b2007d5322d5109
Aditya <aditya.sasmal.dev@gmail.com>
2024-09-23 00:31:42 +0530

    feat: learn page
    learn page with sticky header and sidebar with user progress

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,5 +1,26 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sitcky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
+import { Header } from "./header";
+
 const learnPage = () => {
-  return <div>learn Page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourses={{ title: "Mandarin", imgSrc: "/tw.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Mandarin" />
+        <div className="space-y-4"></div>
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default learnPage;
